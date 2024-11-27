86a1b73ed2a8f2fc040288169e04cefa35314c41
JorgeAndres-GitHub <jorgeandresherreramonsalve@gmail.com>
2024-11-26 20:22:34 -0500

    Agregando imagen del grupo

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import '../css/About.css';
+import ContactTeamImage from '../assets/grupo.jpg'
 
 export function About() {
   const handleShowAlert = () => {
     Swal.fire({
-      title: "Información del Grupo",
-      html: `
-        <strong>Grupo de Trabajo:</strong> <br>
-        - Jorge Andrés Herrera Monsalve - <a href="mailto:jorgherrera@utb.edu.co" target="_blank" rel="noopener noreferrer">jorgherrera@utb.edu.co</a> <br>
-        - María Alejandra Flórez Grau - <a href="mailto:maflorez@utb.edu.co" target="_blank" rel="noopener noreferrer">maflorez@utb.edu.co</a> <br>
-        - Juan Camilo Barboza Martinez - <a href="mailto:jubarboza@utb.edu.co" target="_blank" rel="noopener noreferrer">jubarboza@utb.edu.co</a> <br>
-        - Jorge Florez - <a href="mailto:jubarboza@utb.edu.co" target="_blank" rel="noopener noreferrer">jorflorez@utb.edu.co</a><br>
-      `,
-      icon: "info",
-      confirmButtonText: "Entendido",
-      confirmButtonColor: "#28a745",
+        title: "Información del Grupo",
+        html: `
+            <div class="contact-modal-content">
+                <img src="${ContactTeamImage}" alt="Equipo de Trabajo" class="contact-team-image" />
+                <div class="contact-info">
+                    <strong>Grupo de Trabajo:</strong> <br>
+                    - Jorge Andrés Herrera Monsalve <a href="mailto:jorgherrera@utb.edu.co" target="_blank" rel="noopener noreferrer">jorgherrera@utb.edu.co</a> <br>
+                    - María Alejandra Flórez Grau <a href="mailto:maflorez@utb.edu.co" target="_blank" rel="noopener noreferrer">maflorez@utb.edu.co</a> <br>
+                    - Juan Camilo Barboza Martinez <a href="mailto:jubarboza@utb.edu.co" target="_blank" rel="noopener noreferrer">jubarboza@utb.edu.co</a> <br>
+                    - Jorge Florez - <a href="mailto:jubarboza@utb.edu.co" target="_blank" rel="noopener noreferrer">jorflorez@utb.edu.co</a><br>
+                </div>
+            </div>
+        `,
+        icon: "info",
+        confirmButtonText: "Entendido",
+        confirmButtonColor: "#28a745",
+        width: '600px',
     });
   };
 
